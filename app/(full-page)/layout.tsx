c01eabcfcf2eeee8e7939ef2d5d3c1a5e95b8cf6
nattawat_sit <nattawat@sense-infotech.com>
2025-05-04 11:37:40 +0700

    feature: change theme title to template TS

--- app/(full-page)/layout.tsx
+++ app/(full-page)/layout.tsx
@@ -7,7 +7,7 @@ interface SimpleLayoutProps {
 }
 
 export const metadata: Metadata = {
-    title: 'PrimeReact Sakai',
+    title: 'template TS',
     description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.'
 };
 
